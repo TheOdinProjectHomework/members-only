450dee67dd0865d62827acfa55a9b70bed3f07ee
Cristian Contreras <contreras.cristian994@gmail.com>
2026-03-31 01:55:03 -0400

    update memberStatus added

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -37,3 +37,24 @@ export const editUserInfo = async (req, res) => {
         return res.status(404).json({ success: false, error: error.message });
     }
 }
+
+export const updateStatus = async (req, res) => {
+    const { id, word } = req.body;
+    if(word !== process.env.SECRETWORD) {
+        return res.status(401).json({ success: false, message: "Invalid code" });
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(id, { memberStatus: true }, { new: true });
+        if(!user) return res.status(404).json({ success: false, message: "User not found!"});
+        res
+          .status(200)
+          .json({
+            success: true,
+            message: "Welcome, you have full access now!",
+          });
+    } catch (error) {
+        console.log("Error updating user status");
+        return res.status(500).json({ success: false, error: error.message });    
+    }
+}
